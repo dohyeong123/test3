6fab8d217238ca07eaac4dd1520660b4203bc432
sudond <sudond@naver.com>
2019-08-08 13:42:37 +0900

    cc

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0t#=p*cq59yykah$ese5&mp7ph0s6!z=5j=+scf13r=b-xe1gs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.phnmeqwavk.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env3.phnmeqwavk.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
